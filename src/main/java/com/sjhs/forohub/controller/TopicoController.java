44c38e83be4e1fcf4e48ee2b2c9878205bdf83c1
Santiago Hoyos <santiagojhoyoss@gmail.com>
2026-03-10 03:15:21 -0500

    feat/list all topicos done

--- src/main/java/com/sjhs/forohub/controller/TopicoController.java
+++ src/main/java/com/sjhs/forohub/controller/TopicoController.java
@@ -1,9 +1,14 @@
 package com.sjhs.forohub.controller;
 
+import com.sjhs.forohub.domain.topico.ListarTopicosService;
 import com.sjhs.forohub.domain.topico.RegistrarTopicoDTO;
 import com.sjhs.forohub.domain.topico.RegistrarTopicoService;
+import com.sjhs.forohub.domain.topico.TopicoRespuestaDTO;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +20,12 @@ public class TopicoController {
     @Autowired
     private RegistrarTopicoService registrarTopicoService;
 
+    @Autowired
+    private ListarTopicosService listarTopicosService;
+
     @GetMapping
-    public String helloWorld() {
-        return "Hello world from Europe!";
+    public ResponseEntity<Page<TopicoRespuestaDTO>> listar(@PageableDefault(size = 2) Pageable paginacion) {
+        return ResponseEntity.ok(listarTopicosService.listar(paginacion));
     }
 
     @PostMapping
--- src/main/java/com/sjhs/forohub/domain/topico/TopicoRepository.java
+++ src/main/java/com/sjhs/forohub/domain/topico/TopicoRepository.java
@@ -1,6 +1,9 @@
 package com.sjhs.forohub.domain.topico;
 
+import com.sjhs.forohub.domain.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 }
